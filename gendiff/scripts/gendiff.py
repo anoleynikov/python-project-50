4cd0642c99601f4ce24c812a01f89913fff852ed
anton9760 <anton2610@mail.ru>
2024-10-04 17:15:02 +0300

    venv create

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,15 @@
-import argparse
+#!/usr/bin/env python
+"""Difference Generator."""
+
+from gendiff import generate_diff
+from gendiff.cli import parsing_args
+
 
 def main():
-    """Parsing args from user command line"""
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        description="Compares two configuration files "
-                    "and shows a difference.")
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format',
-                        choices=["stylish", "plain", "json"],
-                        default="stylish",
-                        help='set format of output')
-    args = parser.parse_args()
-    return args.first_file, args.second_file, args.format
+    """Get start here."""
+    first_file, second_file, output_format = parsing_args()
+    print(generate_diff(first_file, second_file, output_format))
+
 
 if __name__ == '__main__':
     main()
